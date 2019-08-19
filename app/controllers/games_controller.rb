ffd811c29525d59e9c4b303bc25e01b68a889e09
Michael Wang <michaelw0395@gmail.com>
2019-08-19 17:07:07 +1000

    Completed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,37 @@
+require 'json'
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    alphabet_array = ("A".."Z").to_a
+    10.times {
+      @letters.push(alphabet_array.sample)
+    }
+  end
+
+  def score
+    @letters = params[:grid]
+    @word = params[:word]
+    # first test - "The word can't be built out of the original grid"
+    @word.split("").each do |letter|
+      if @letters.include?(letter.upcase)
+        @result = "yes"
+        # second test - Is a valid English word
+        filepath = open("https://wagon-dictionary.herokuapp.com/#{@word}")
+
+        word_hash = JSON.parse(filepath.read)
+        # if word is valid word
+        if word_hash["found"] == true
+          return @result = "YAS"
+        # else sorry not a valid word
+        else
+          return @result = "Sorry"
+        end
+
+      else
+        return @result = "Sorry but #{@word.upcase} can't be built out of #{@letters[0...-1].gsub(/([A-Z])/, '\1, ') + @letters[-1]}"
+      end
+    end
+  end
+end
